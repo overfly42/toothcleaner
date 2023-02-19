ad8a7125ad86900d9b5f9ef8c049c347b0fb18c2
Christian Spreng <c.spreng@zoho.com>
2023-02-19 09:21:54 +0100

    Added status message for test circuit

--- test_cicuit.py
+++ test_cicuit.py
@@ -9,5 +9,5 @@ def cb(value):
     print('Got an edge from {}.'.format(value))
 GPIO.add_event_detect(channel,GPIO.RISING,callback=cb)
 for i in range(20):
-    print('Going to sleep')
+    print('Going to sleep, current state is {}'.format(GPIO.input(channel)))
     sleep(1)
